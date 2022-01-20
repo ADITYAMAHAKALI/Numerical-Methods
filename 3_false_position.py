d0d70be555b789957c509352dde584b4ec1d0da2
ADITYAMAHAKALI <adityamahakali@gmail.com>
2022-01-20 15:00:36 +0530

    sfds

--- 3_false_position.py
+++ 3_false_position.py
@@ -1,7 +1,8 @@
 import math
+from operator import ne
 #function definition
 def func(x):
-    return x**3- 3*x +1 
+    return  x**4 - x -10
 
 def build_table(range_start,range_end,increment):
     x = range_start
@@ -23,7 +24,7 @@ def find_most_appropriate_points(X,Y):
             #making_sure that x0, x1 are of least magnitude
             if(abs(X[i]*X[i+1])<= least_magnitude):
                 best_start = i
-                least_magnitude = X[i]*X[i+1]
+                least_magnitude = abs(X[i]*X[i+1])
     return X[best_start],X[best_start+1]
 
 def find_root_false_position_with_iterations(x0,x1,iteration):
@@ -43,18 +44,25 @@ def find_root_false_position_with_iterations(x0,x1,iteration):
         #if root liews between x0 and new_mid_x
         if(func(x0)*f_new<0):
             print(f"Since, f({x0}) f({new_mid_x}) < 0, the root lies in the interval ({x0}, {new_mid_x})")
-            prev_x = x1 
+            prev_x = x1
             x1 = new_mid_x
-        else:
+             
+        elif(func(x1)*f_new<0):
             print(f"Since, f({new_mid_x}) f({x1}) < 0, the root lies in the interval ({new_mid_x}, {x1})")
             prev_x = x0 
             x0 = new_mid_x
+        else:
+            print("intermediate value theorem failed!! Possible wrong range of values")
+        
         iteration = iteration - 1 
-    return prev_x,new_mid_x
+        if(abs(new_mid_x - prev_x)< 1e-6):
+            print(f"|{new_mid_x}| - {prev_x}|<{1e-6}")
+            return new_mid_x
+    return new_mid_x
 
 def printTable(X,Y):
     l = len(X)
-    print("Value Table Baesd on function: ")
+    print("Value Table Based on function: ")
     print("X","\t","Y")
     for i in range(l):
         print(X[i],"\t",Y[i])
@@ -62,15 +70,15 @@ def printTable(X,Y):
 def main():
     X=[]
     Y=[]
-    X,Y = build_table(-5,5,1)
+    X,Y = build_table(0,3,0.2)
     printTable(X,Y)
     # determining best x0,x1
     x0,x1 = find_most_appropriate_points(X,Y)
     print("Best x0,x1: ")
     print(x0,x1)
     # now initiating false position method
-    x0,x1 = find_root_false_position_with_iterations(x0,x1,10)
-    print(x0,x1)
+    root = find_root_false_position_with_iterations(x0,x1,20)
+    print("Required value of root: ",root)
 
 
 if __name__ == "__main__":
